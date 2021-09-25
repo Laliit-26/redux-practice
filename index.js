e73463cc009ef95e4ab550582922025bf74f68bc
lalit <tiwarilalit2601@gmail.com>
2021-09-25 17:30:40 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const BUY_CAKE = 'BUY_CAKE';
+const redux = require('redux');
+const createStore = redux.createStore;
+
 
 
 function buycake() {
     return {
-    type:BUY_CAKE
+    type:'BUY_CAKE'
 }
 }
 
@@ -11,13 +13,24 @@ const initialstate = {
     numofcakes: 10
 }
 const reducer = (state = initialstate, action) => {
+    console.log(action.type === 'BUY_CAKE')
     switch (action.type)
-    {
-        case BUY_CAKE:
+    { 
+        case 'BUY_CAKE':
             return {
                 ...state,
-                numofcakes: state.numofcakes - 1;
+                numofcakes: state.numofcakes - 1 
+            
             }
         default:return state
     }
 }
+
+const store = createStore(reducer);
+console.log(store);
+console.log('initial state' , store.getState());
+const unsuscribe=store.subscribe(() => console.log('updated state' , store.getState()))
+store.dispatch(buycake());
+store.dispatch(buycake());
+store.dispatch(buycake());
+unsuscribe();
